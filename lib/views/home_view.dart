6b647e6f81cfedf46a3cc627b879ba6c97acfce6
구본국 <kook9@gubongug-ui-Macmini.local>
2021-06-08 15:18:00 +0900

    최종:mobile 화면 추가.

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flweb_layout/views/home_content_desktop.dart';
 import 'package:flweb_layout/views/home_content_mobile.dart';
 import 'package:flweb_layout/widgets/navigation_bar/navigation_bar.dart';
 import 'package:flweb_layout/widgets/centered_view/centered_view.dart';
+import 'package:flweb_layout/widgets/navigation_drawer/navigation_drawer.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 class HomeView extends StatelessWidget {
@@ -10,21 +11,25 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: CenteredView(
-        child: Column(
-          children: <Widget>[
-          NavigationBar(),
-          Expanded(
-            child: ScreenTypeLayout(
-              mobile: HomeContentMobile(),
-              desktop: HomeContentDesktop(),
-            ),
-          )
-        ],
-      ),
-    )
-    );
+    return ResponsiveBuilder(
+        builder: (context, sizingInformation) => Scaffold(
+            drawer:
+                sizingInformation.deviceScreenType == DeviceScreenType.mobile
+                    ? NavigationDrawer()
+                    : null,
+            backgroundColor: Colors.white,
+            body: CenteredView(
+              child: Column(
+                children: <Widget>[
+                  NavigationBar(),
+                  Expanded(
+                    child: ScreenTypeLayout(
+                      mobile: HomeContentMobile(),
+                      desktop: HomeContentDesktop(),
+                    ),
+                  )
+                ],
+              ),
+            )));
   }
 }
